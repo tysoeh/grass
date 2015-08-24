90ebbebfdd6abbb433586868e20b7329713152bc
Ty Soehngen <ty.soeh@gmail.com>
2015-08-24 10:19:16 -0400

    Lawn successfully grows randomly for any specified number of blades of grass

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ function Blade (instanceNum){
   this.instanceNum = instanceNum;
   var blade = document.getElementById("blade");
   var bladeClone = blade.cloneNode(true);
-  bladeClone.id = "blade" + instanceNum; //in case we need to call this instance in the future
-  console.log(bladeClone);
-  var bladeHeight = bladeClone.style.height;
-  bladeClone.style.left = instanceNum; //sets absolute position from the left equal to its instance number
-  
+    console.log(bladeClone);
+    bladeClone.id = "blade" + instanceNum; //in case we need to call this instance in the future
+  document.getElementById("lawn").appendChild(bladeClone); //attach this instance to the lawn
+  var bladeHeightValue = 0;
+  var bladeHeight = bladeClone.style.height = bladeHeightValue + "px";
+    console.log(bladeClone.style.height);
+  bladeClone.style.color = "green";
+  bladeClone.style.position = "absolute";
+  bladeClone.style.left = instanceNum + "px"; //sets absolute position from the left equal to its instance number
+    console.log(bladeClone.style.left);
+    
   //bladeClone.style.color = bladeColor; //mess with this later- randomize shade of green
   //var bladeColor = randomizer();
   
-  var bladeGrowRate = Math.floor(Math.random() * 1000 + 1); //sets a random number between 1 and 1000
-  window.setInterval(this.grow(), bladeGrowRate); //calls the grow function for this instance at an interval of every bladGrowRate seconds
-  
-  //cycle through all grass elements and assign each to a new "blade" object with
-  //a starting height of 0, random grow rate and shade of green. each object calls the grow function
-  //with their grow rate as a parameter
+  var bladeGrowRate = Math.floor(Math.random() * 300 + 150); //sets a random number between 149 and 449
+  setInterval(function(){grow()}, bladeGrowRate); //calls the grow function at the rate of whatever random number was just assigned
 
 
+function grow(){ //makes blade increase in height by a pixel
+  bladeHeightValue ++;
+  bladeHeight = bladeClone.style.height = bladeHeightValue + "px";
+}
   
 }
-Blade (10);
+for (i=0; i<=449; i++){ //create "i" number of blades
+Blade(i);
+document.getElementById("soil").style.width = i + 1 + "px"; //make the soil as wide as the number of blades of grass
+}
 
-//for 1-50 call Blade(instanceNum++)
-
-Blade.prototype.grow = function(){
-  this.bladeHeight ++; //makes blade increase in height by a pixel
-};
