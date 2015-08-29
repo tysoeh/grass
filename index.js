3a5c8295570b1419639a2c1b48628bc2c46dff44
Ty Soehngen <ty.soeh@gmail.com>
2015-08-29 14:50:56 -0400

    WIP This commit creates a soil element and lawn element for each h2 element it finds on the page

--- index.js
+++ index.js
@@ -1,28 +1,50 @@
-function blade() {}
+var beds = document.getElementsByTagName("h2");
+  console.log(beds);
 
-var lawn = document.getElementById('lawn');
-
-for (var i = 0; i <= 50; i++) {
-    var newBlade = document.createElement('div');
-    lawn.appendChild(newBlade);
-    newBlade.className = "blade";
-    console.log(newBlade);
-};
-
-
+//var lawn = document.getElementById('lawn');
 var blades = document.getElementsByClassName('blade');
-console.log(blades);
-
+//console.log(blades);
 setInterval(function() {
-        for (var i = 0; i < blades.length; i++) {
+        for (var i = 0; i < blades.length; i++){
             var bladeHeight = blades[i].clientHeight;
             bladeHeight += Math.floor((Math.random() * 10) + 1);
             blades[i].style.height = bladeHeight + 'px';
-            console.log(bladeHeight);
+            //console.log(bladeHeight);
         }
     },
     2000);
+    
 
+
+function makeGardenBeds(){  //for each h2 element, make a div above (lawn) and a div below (soil)
+  
+    for (var i = 0; i<beds.length; i++){ //should "i" start at 0 or 1?
+      var newSoil = document.createElement('span'); //may need to be span
+      newSoil.className = "soil";
+      beds[i].appendChild(newSoil);
+      console.log(newSoil);
+      
+      var newLawn = document.createElement('span');
+      newLawn.className = "lawn";
+      beds[i].appendChild(newLawn);
+      console.log(newLawn);
+    }
+}
+
+/*function seedGardenBeds(){  //for each lawn element, make blades of grass
+  for (var i = 0; i<beds.length; i++){
+    
+    for (var j = 0; j <= 50; j++) {
+      var newBlade = document.createElement('div');
+      thisLawn.appendChild(newBlade);
+      newBlade.className = "blade";
+      //console.log(newBlade);
+    }
+  }
+}*/
+
+makeGardenBeds();
+//seedGardenBeds();
 
 // function Blade(instanceNum) {
 //     this.instanceNum = instanceNum;
