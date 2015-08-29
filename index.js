f411f8408ba5546b2789bb54b334af70fed71f4a
Ty Soehngen <ty.soeh@gmail.com>
2015-08-29 15:23:11 -0400

    WIP this commit creates soil and growing grass for each h2 element it finds on the page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var beds = document.getElementsByTagName("h2");
+var beds = document.getElementsByTagName("h2"); //get a list of the h2 elements on DOM
   console.log(beds);
 
 //var lawn = document.getElementById('lawn');
@@ -28,63 +28,20 @@ function makeGardenBeds(){  //for each h2 element, make a div above (lawn) and a
       newLawn.className = "lawn";
       beds[i].appendChild(newLawn);
       console.log(newLawn);
+      
+      seedGrass(newLawn);
     }
 }
 
-/*function seedGardenBeds(){  //for each lawn element, make blades of grass
-  for (var i = 0; i<beds.length; i++){
-    
-    for (var j = 0; j <= 50; j++) {
+function seedGrass(lawn){  //for each lawn element, make blades of grass
+      this.lawn = lawn;
+      for (var j = 0; j <= 50; j++) {
       var newBlade = document.createElement('div');
-      thisLawn.appendChild(newBlade);
+      lawn.appendChild(newBlade);
       newBlade.className = "blade";
       //console.log(newBlade);
-    }
+    
   }
-}*/
+}
 
 makeGardenBeds();
-//seedGardenBeds();
-
-// function Blade(instanceNum) {
-//     this.instanceNum = instanceNum;
-//     // var blade = document.getElementById("blade");
-//     var bladeClone = blade.cloneNode(true);
-//     console.log(bladeClone);
-//     bladeClone.id = "blade" + instanceNum; //in case we need to call this instance in the future
-//     document.getElementById("lawn").appendChild(bladeClone); //attach this instance to the lawn
-//     var bladeHeightValue = 0;
-//     var bladeHeight = bladeClone.style.height = bladeHeightValue + "px";
-//     console.log(bladeClone.style.height);
-//     bladeClone.style.color = "green";
-//     bladeClone.style.position = "absolute";
-//     bladeClone.style.left = instanceNum + "px"; //sets absolute position from the left equal to its instance number
-//     console.log(bladeClone.style.left);
-
-//     //bladeClone.style.color = bladeColor; //mess with this later- randomize shade of green
-//     //var bladeColor = randomizer();
-
-//     var bladeGrowRate = Math.floor(Math.random() * 300 + 150); //sets a random number between 149 and 449
-//     setInterval(function() {
-//         grow()
-//     }, bladeGrowRate); //calls the grow function at the rate of whatever random number was just assigned
-
-
-//     function grow() { //makes blade increase in height by a pixel
-//         bladeHeightValue++;
-//         bladeHeight = bladeClone.style.height = bladeHeightValue + "px";
-//     }
-
-// }
-// for (i = 0; i <= 449; i++) { //create "i" number of blades
-//     Blade(i);
-//     document.getElementById("soil").style.width = i + 1 + "px"; //make the soil as wide as the number of blades of grass
-// }
-// bladeHeightValue++;
-// bladeHeight = bladeClone.style.height = bladeHeightValue + "px";
-// }
-// }
-// for (i = 0; i <= 200; i++) { //create "i" number of blades
-//     Blade(i);
-//     document.getElementById("soil").style.width = i + 1 + "px"; //make the soil element as wide as the number of blades of grass
-// }
